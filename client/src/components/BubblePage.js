a584bacf292361da4a8db2bf53343dea0ce1fdc1
nick.durbin <nick.durbin@gmail.com>
2019-11-08 20:36:48 -0500

    A server restart fixed the delete functionality. MVP complete.

--- client/src/components/BubblePage.js
+++ client/src/components/BubblePage.js
@@ -6,8 +6,6 @@ import ColorList from "./ColorList";
 const BubblePage = () => {
   const [colorList, setColorList] = useState([]);
   const [refresh, setRefresh] = useState(false);
-  // fetch your colors data from the server when the component mounts
-  // set that data to the colorList state property
 
   useEffect(() => {
     axios().get('/api/colors')
